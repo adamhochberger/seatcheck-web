642828d5521c258d7aa99afbc0a5bce951469155
Adam Hochberger <adamhochberger@gmail.com>
2020-01-23 14:31:02 -0500

    basic react app

--- src/index.js
+++ src/index.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+import './index.css';
 import App from './App';
+import * as serviceWorker from './serviceWorker';
 
-const title = 'My Minimal React Webpack Babel Setup';
+ReactDOM.render(<App />, document.getElementById('root'));
 
-ReactDOM.render(
-  <App title={title} />,
-  document.getElementById('app')
-);
-
-module.hot.accept();
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
