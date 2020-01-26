f3e7f19b178f2971e39c7828d6caa8031bc9e52b
Raymond jones <Rayjones2170@gmail.com>
2020-01-26 14:27:46 -0500

    Feature: Adds more basic shapes

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
 import Konva from 'konva';
 import { render } from 'react-dom';
-import { Stage, Layer, Star, Text } from 'react-konva';
+import { Stage, Layer, Star, Text, Rect, Circle, Line  } from 'react-konva';
+
+
 
 class App extends Component {
   handleDragStart = e => {
@@ -26,7 +28,36 @@ class App extends Component {
   };
   render() {
     return (
+
+
+
+
       <Stage width={window.innerWidth} height={window.innerHeight}>
+
+        <Layer>
+          <Text text="Some text on canvas" fontSize={15} />
+          <Rect
+            x={20}
+            y={50}
+            width={100}
+            height={100}
+            fill="red"
+            shadowBlur={10}
+          />
+          <Circle x={200} y={100} radius={50} fill="green" />
+          <Line
+            x={20}
+            y={200}
+            points={[0, 0, 100, 0, 100, 100]}
+            tension={0.5}
+            closed
+            stroke="black"
+            fillLinearGradientStartPoint={{ x: -50, y: -50 }}
+            fillLinearGradientEndPoint={{ x: 50, y: 50 }}
+            fillLinearGradientColorStops={[0, 'red', 1, 'yellow']}
+          />
+        </Layer>
+
         <Layer>
           <Text text="Try to drag a star" />
           {[...Array(10)].map((_, i) => (
