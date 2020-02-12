edc62ed5b38a31660438ae4e976a12647c442ca7
Raymond jones <Rayjones2170@gmail.com>
2020-02-12 13:48:23 -0500

    Feature: Adds user select options to edit the grid they create

--- src/MapBuild/buildmap.js
+++ src/MapBuild/buildmap.js
@@ -4,63 +4,99 @@ class buildMapView extends React.Component {
     constructor(props) {
         super(props);
         this.state = {grid: [], curr:'X'};
-        this.setPoint = this.setPoint.bind(this);        
-        this.handleChange = this.handleChange.bind(this);
-        this.uploadMap = this.uploadMap.bind(this);
+        this.setPoint = this.setPoint.bind(this);
+        this.changeToWall = this.changeToWall.bind(this);        
+        this.changeToDoor = this.changeToDoor.bind(this);        
+        this.changeToTable = this.changeToTable.bind(this);        
+        this.changeToElevator = this.changeToElevator.bind(this); 
+        this.changeToStairs = this.changeToStairs.bind(this);        
+        this.submitMap = this.submitMap.bind(this);        
+
         
         const initializeGrid = () => {
-          for (let i = 0; i < 5; i++) {
+          for (let i = 0; i < 10; i++) {
             this.state.grid.push([]);
-            for (let j = 0; j < 5; j++){
+            for (let j = 0; j < 10; j++){
               this.state.grid[i].push('O');
             }
           }
         };
-
         initializeGrid();
-        console.log(this.state.grid);
+
       }
       setPoint(event) {
-        //Col
         var col = event.target.getAttribute("id");
-        console.log(col);
-        //Row
         var row = event.target.getAttribute("class");
-        console.log(row);
-        //console.log(this.state.grid[row][col])
-        
         this.state.grid[row][col] = this.state.curr;
         this.setState(this.state.grid);
-        
-        //console.log(this.state.grid[row][col])
-        //console.log(this.state.grid);
-        //this.forceUpdate();
-
-        //this.setState({value: event.target.value});
       }
-      handleChange(event) {
+      changeToWall(event) {
+        this.state.curr = "w";
+        //this.setState(this.state.curr);
         //console.log(event.target.getAttribute("id"));
         //this.setState({grid[event.target.row][event.target.cellId]: event.target.value});
       }
-      
-      uploadMap(event) {
-        alert('A name was submitted: ' + this.state.value);
-        event.preventDefault();
+      changeToDoor(event){
+        this.state.curr = "d";
+        //this.setState(this.state.curr);
+      }
+      changeToTable(event) {
+        this.state.curr = "t";
+        //this.setState(this.state.curr);
+        //console.log(event.target.getAttribute("id"));
+        //this.setState({grid[event.target.row][event.target.cellId]: event.target.value});
+      }
+      changeToElevator(event){
+        this.state.curr = "e";
+        //this.setState(this.state.curr);
+      }
+      //Code here to send current grid layout to firebase under user credentials
+      submitMap(event) {
+        this.state.curr = "w";
+        //this.setState(this.state.curr);
+        //console.log(event.target.getAttribute("id"));
+        //this.setState({grid[event.target.row][event.target.cellId]: event.target.value});
+      }
+      changeToStairs(event){
+        this.state.curr = "s";
+        //this.setState(this.state.curr);
       }
       
       render() {  
 
         return (
           <div>
-              <form onSubmit={this.uploadMap}>
                 <label>
-                  Select Areas to encode:
+                  Select Areas to encode (Choose an option):
                 </label>
-                <input type="button" name="name" />
-                <input type="button" name="name" />
-                <input type="button" name="name" />
+                <br>
+                </br>
+                <br>
+                </br>
+                <label onClick={this.changeToWall}>
+                  Wall:
+                </label>
+                
+                <label onClick={this.changeToDoor}>
+                  Door:
+                </label>
 
-              </form>
+                <label onClick={this.changeToTable}>
+                  Table:
+                </label>
+                
+                <label onClick={this.changeToElevator}>
+                  Elevator:
+                </label>
+
+                <label onClick={this.changeToStairs}>
+                  Stairs:
+                </label>
+                
+                <label onClick={this.submitMap}>
+                  Submit
+                </label>
+
               {/*}
                 <ol>
                   {this.state.grid.map(obj => <li>{obj}</li>)}
