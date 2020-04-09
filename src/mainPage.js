8fa0ab40b8925a83c440d93653278f085c58733e
Adam Hochberger <adamhochberger@gmail.com>
2020-04-09 14:55:57 -0400

    picture and addition of landing page

--- src/mainPage.js
+++ src/mainPage.js
@@ -1,32 +1,37 @@
 import React from 'react';
-import logo2 from './assets/./logo.png';
 import './index.css';
-import MyFilteringComponent from './filter.js';
-import Login from './Login/login.js'
 
-import {
-  BrowserRouter as Router,
-} from "react-router-dom";
+import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
+import Grid from "@material-ui/core/Grid";
+
 class MainPage extends React.Component {
       render() {
-        const users =
-        [
-            "Tom",
-            "Barry",
-            "Dave",
-            "John"
-        ];
-        return (
-          <Router>
-            <div className="App">
-            <header className="App-header">
-              <p>
-                Welcome to Social Compass.
-              </p>
 
-            </header>
+        return (
+            <div className="background-img">
+
+                <Typography class="page-header" variant="h1" component="h1">
+                    Welcome to Social Compass
+                </Typography>
+                <Typography class="page-text" variant="body1" component ="body1">
+                A seating platform for <br/>professionals and students <br/><br/>
+                    To begin, create an account by clicking on the profile icon <br/>
+                    Afterwards, the left menu lets you access the website features. <br/>
+                    Or, you can access those pages through these buttons:<br/>
+                </Typography>
+                <br/>
+
+                <Grid  xs={12} md={12} lg={12}>
+                    <Button variant="contained" color="primary" href="/create">
+                            Create map
+                    </Button>
+                    <Button variant="contained" color="primary" href="/view">
+                            View map
+                    </Button>
+                
+                </Grid>
             </div>
-          </Router>
         );
       }
 
